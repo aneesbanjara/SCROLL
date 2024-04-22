e296680fdd2d188596301f4cc085ddcdf58d8a49
Anish <aneesbanjara488@gmail.com>
2024-04-22 12:01:32 +0545

    add: deployed workflows

--- vite.config.js
+++ vite.config.js
@@ -4,23 +4,6 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, "src/App.jsx"),
-      name: "scroll",
-      fileName: (format) => `scroll.${format}.js`,
-    },
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-        },
-      },
-    },
-    sourcemap: true,
-    emptyOutDir: true,
-  },
   plugins: [react()],
   server: {
     port: 3000,
